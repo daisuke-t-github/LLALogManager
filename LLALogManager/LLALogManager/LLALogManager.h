df139943d0446bf5abc0de6757a8768b24983edf
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-08 23:00:32 +0900

    Add MacOS scheme

--- LLALogManager/LLALogManager/LLALogManager.h
+++ LLALogManager/LLALogManager/LLALogManager.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Daisuke T. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for LLALogManager.
 FOUNDATION_EXPORT double LLALogManagerVersionNumber;
