1c7cf35df1f7bded3f76b2e0c64e373bbda5ec23
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-08 23:02:26 +0900

    Add macOS demo

---
+++ demo/LLALogManagerDemoMacOS/LLALogManagerDemoMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LLALogManagerDemoMacOS
+//
+//  Created by Daisuke T on 2019/02/08.
+//  Copyright © 2019 LLALogManagerDemoMacOS. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
